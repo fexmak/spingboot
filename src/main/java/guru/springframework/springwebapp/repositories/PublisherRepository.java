9a36646ac0ebd4d75b4dccd696e6ca28985589b9
fexmak <fexmak@gmail.com>
2020-06-15 16:53:33 -0300

    new publisher spring repo

---
+++ src/main/java/guru/springframework/springwebapp/repositories/PublisherRepository.java
@@ -0,0 +1,12 @@
+package guru.springframework.springwebapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import guru.springframework.springwebapp.model.Publisher;
+
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long>{
+
+	
+	
+}
